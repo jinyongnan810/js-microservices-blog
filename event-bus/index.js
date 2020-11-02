9581298c2e9ebe65e398e99b4e8ddaa640b60d94
Yuunan Kin <yuunan.kin@gmail.com>
2020-11-02 11:01:53 +0900

    sync events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,13 +5,36 @@ const { default: Axios } = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", async (req, res) => {
   const event = req.body;
-  await Axios.post("http://localhost:4000/events", event);
-  await Axios.post("http://localhost:4001/events", event);
-  await Axios.post("http://localhost:4002/events", event);
-  await Axios.post("http://localhost:4003/events", event);
+  events.push(event);
+  try {
+    await Axios.post("http://localhost:4000/events", event);
+  } catch (error) {
+    console.log("Posts service down.");
+  }
+  try {
+    await Axios.post("http://localhost:4001/events", event);
+  } catch (error) {
+    console.log("Comments service down.");
+  }
+  try {
+    await Axios.post("http://localhost:4002/events", event);
+  } catch (error) {
+    console.log("Query service down.");
+  }
+  try {
+    await Axios.post("http://localhost:4003/events", event);
+  } catch (error) {
+    console.log("Moderation service down.");
+  }
   res.send({});
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -9,14 +9,7 @@ app.use(bodyParser.json());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(Object.values(posts));
-});
-
-app.post("/events", (req, res) => {
-  const event = req.body;
-  const { type, content } = event;
-
+const handleEvents = (type, content) => {
   switch (type) {
     case "Post-Created":
       posts[content.id] = { ...content, comments: {} };
@@ -31,9 +24,26 @@ app.post("/events", (req, res) => {
     default:
       break;
   }
+};
+
+app.get("/posts", (req, res) => {
+  res.send(Object.values(posts));
+});
+
+app.post("/events", (req, res) => {
+  const event = req.body;
+  const { type, content } = event;
+  handleEvents(type, content);
+
   res.send({});
 });
 
-app.listen("4002", () => {
+app.listen("4002", async () => {
   console.log("Listening on 4002");
+
+  //fetching all the events
+  const res = await axios.get("http://localhost:4005/events");
+  res.data.map((event) => {
+    handleEvents(event.type, event.content);
+  });
 });
